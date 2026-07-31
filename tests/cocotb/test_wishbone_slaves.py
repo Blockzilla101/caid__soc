7d7e1ad9d056eb61fad7ee65bb8d8fbdc2b9332a
Blockzilla101 <blockzilla101@gmail.com>
2026-07-31 10:51:46 +0500

    add assertions for memory slave

--- tests/cocotb/test_wishbone_slaves.py
+++ tests/cocotb/test_wishbone_slaves.py
@@ -17,37 +17,10 @@ def reset_bus(dut):
 async def test_wb_mem(dut):
     """Test wishbone memory"""
 
-    while get_sim_time("ns") < 500:
+    while get_sim_time("ns") < 100:
         await Timer(1, "ns")
 
-    # reset_bus(dut)
-    # await setup_clock_sig(dut.wb_CLK_I, dut.wb_RST_I)
-
-    # dut.wb_CYC_I.value = 1
-    # dut.wb_STB_I.value = 1
-    # dut.wb_WE_I.value = 1
-    # dut.wb_ADR_I.value = 0
-    # dut.wb_SEL_I.value = 0xF
-    # dut.wb_DAT_I.value = 0xA0B0C0D0
-
-    # await dut.wb_CLK_I.rising_edge
-    # await dut.wb_CLK_I.rising_edge
-
-    # reset_bus(dut)
-
-    # await dut.wb_CLK_I.rising_edge  #
-
-    # await dut.wb_CLK_I.rising_edge
-
-    # dut.wb_CYC_I.value = 1
-    # dut.wb_STB_I.value = 1
-    # dut.wb_WE_I.value = 0
-    # dut.wb_ADR_I.value = 0
-    # dut.wb_SEL_I.value = 0xF
-
-    # await dut.wb_CLK_I.rising_edge
-    # await dut.wb_CLK_I.rising_edge
-
-    # reset_bus(dut)
-
-    # await dut.wb_CLK_I.rising_edge
+    assert dut.wb_data_mem.memory[3].value == 0xA0
+    # assert dut.wb_data_mem.memory[2].value == 0x00
+    # assert dut.wb_data_mem.memory[1].value == 0x00
+    assert dut.wb_data_mem.memory[0].value == 0xD0
