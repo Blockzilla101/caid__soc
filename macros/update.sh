d364d6760f483a8a6322c98798716dc212eb12fb
Blockzilla101 <blockzilla101@gmail.com>
2026-08-15 22:25:58 +0500

    add sram macro

---
+++ macros/update.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+
+if ! (env | grep -q ^OPENRAM_BUILD_PATH=)
+then
+    export OPENRAM_BUILD_PATH=../macro-gen/openram/build
+fi
+
+function copy_openram_macro() {
+    name=$1
+    OUT_PATH=$OPENRAM_BUILD_PATH/$name
+
+    rm -v **/$name*
+
+    cp -v $OUT_PATH/$name.gds  gds/$name.gds
+    cp -v $OUT_PATH/$name.lef  lef/$name.lef
+    cp -v $OUT_PATH/$name.v  nl/$name.v
+    cp -v $OUT_PATH/$name_*.lib  lib/
+}
+
+
+copy_openram_macro sram_1rw1r_32x1024
